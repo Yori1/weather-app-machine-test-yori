c73be05e6ed532c758e86cdc6e6911ab7d0df5eb
Yori <yorigr@outlook.com>
2022-08-17 17:53:57 +0200

    added search module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SearchModule } from './search/search.module';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), SearchModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
