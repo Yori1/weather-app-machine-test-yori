d4944354cb51c33e53eef196d660f33c0e5fca09
Yori <yorigr@outlook.com>
2022-08-17 21:57:14 +0200

    implemented getCurrentWeatherInformationByCity

--- src/app/search/container/searching/searching.component.ts
+++ src/app/search/container/searching/searching.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { OpenWeatherService } from 'src/app/shared/open.weather.service';
+import { pipe } from 'rxjs';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'search-searching',
@@ -16,7 +18,11 @@ export class SearchingComponent implements OnInit {
   }
 
   submit() {
-    this.openWeatherService.getCurrentWeatherInformation();
+    this.openWeatherService.getCurrentWeatherInformationByCity(this.searchTerm)
+      .pipe(first())
+      .subscribe(r => {
+        console.log(JSON.stringify(r))
+      });
   }
 
 }
--- src/app/shared/open.weather.service.ts
+++ src/app/shared/open.weather.service.ts
@@ -5,6 +5,7 @@ import { SharedModule } from './shared.module';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { switchMap } from 'rxjs/operators';
+import { BasicWeatherReport } from './models/BasicWeatherReport';
 
 @Injectable({
   providedIn: SharedModule
@@ -13,11 +14,15 @@ export class OpenWeatherService {
 
   constructor(private http: HttpClient) { }
 
-  getCurrentWeatherInformation() {
-
+  getCurrentWeatherInformationByLonLan(lon: number, lat: number): Observable<BasicWeatherReport> {
+    return this.http.get<BasicWeatherReport>(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${environment.openWeatherApiKey}`);
   }
 
-  getDirectGeocodingResult(cityName: string): Observable<DirectGeocodingResult> {
-    return this.http.get<DirectGeocodingResult>(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=10&appid=${environment.openWeatherApiKey}`);
+  getCurrentWeatherInformationByCity(cityName: string): Observable<BasicWeatherReport> {
+    return this.getDirectGeocodingResult(cityName).pipe(switchMap(r => this.getCurrentWeatherInformationByLonLan(r[0].lon, r[0].lat)));
+  }
+
+  getDirectGeocodingResult(cityName: string): Observable<DirectGeocodingResult[]> {
+    return this.http.get<DirectGeocodingResult[]>(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=10&appid=${environment.openWeatherApiKey}`);
   }
 }
