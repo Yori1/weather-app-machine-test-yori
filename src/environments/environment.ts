2bdd799232bd936e1c1055f7e3617fd91e82ba8f
Yori <yorigr@outlook.com>
2022-08-17 19:54:45 +0200

    added api keys in environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  openWeatherApiKey: "e561a18950e378fcc5cccc4271161d41"
 };
 
 /*
